c62d6a6b2323b9b4afb3954b03254c746167fb29
XenoBino <48479476+XenoBino@users.noreply.github.com>
2022-06-21 21:01:51 +0300

    Add: Backup of .vimrc and .vim

--- install.sh
+++ install.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/bash
 
 SCRIPT_PATH="$(dirname -- "$( readlink -f -- "$0"; )")"
+TIMESTAMP="$(date +"%S:%M:%H@%Y-%m-%d")"
+
+mv $HOME/.vimrc $HOME/.vimrc~$TIMESTAMP
+mv $HOME/.vim   $HOME/.vim~$TIMESTAMP
 
 ln -s $SCRIPT_PATH/vimrc.vim $HOME/.vimrc
 ln -snf $SCRIPT_PATH/config $HOME/.vim
