ab120f0bd2a1f7328a9aab5ff9b01efd4f5d2135
XenoBino <48479476+XenoBino@users.noreply.github.com>
2022-09-01 18:14:47 +0300

    feat: Added bspwm configuration

--- bspwm/bspwmrc.sh
+++ bspwm/bspwmrc.sh
@@ -1,17 +1,16 @@
 #!/bin/sh
 
+bspc monitor -d I II III IV V VI VII VIII IX X
+
 picom --daemon
-sxhkd &
-nitrogen --set-scaled ~/.local/share/wallpapers/bspwm-default.jpg
-~/.config/polybar/launch.sh --panels &
-
-# `bspwmrc` but not tracked by version control
-. ~/.config/bspwm/bspwmrc.local
-
 pgrep -x sxhkd > /dev/null || sxhkd &
 
-# I don't even use one
-bspc monitor -d I II III
+nitrogen --set-scaled ~/.local/share/wallpapers/bspwm-default.jpg
+# $HOME/.config/polybar/launch.sh --panels &
+~/.config/polybar/launch.sh --cuts &
+
+# like `bspwmrc` but not tracked by version control
+[ -f $HOME/.config/bspwm/bspwmrc.local ] && . $HOME/.config/bspwm/bspwmrc.local;
 
 bspc config border_width         2
 bspc config window_gap          12
@@ -20,11 +19,4 @@ bspc config split_ratio          0.52
 bspc config borderless_monocle   true
 bspc config gapless_monocle      true
 
-# Some defaults that I don't understand (yet)
-bspc rule -a Gimp desktop='^8' state=floating follow=on
-bspc rule -a Chromium desktop='^2'
-bspc rule -a mplayer2 state=floating
-bspc rule -a Kupfer.py focus=on
-bspc rule -a Screenkey manage=off
-
-bspc rule -a xfce4-taskmanager state=floating
+bspc rule -a "obs:obs:*" desktop="^3" follow=on
