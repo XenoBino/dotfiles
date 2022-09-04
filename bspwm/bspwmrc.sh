a2e8f68cc084b8bda704115fa4c58f0b64eec587
XenoBino <48479476+XenoBino@users.noreply.github.com>
2022-09-04 22:07:42 +0300

    feat(bspwm): Added variables to configure bspwmrc

--- bspwm/bspwmrc.sh
+++ bspwm/bspwmrc.sh
@@ -1,21 +1,39 @@
 #!/bin/sh
 
-bspc monitor -d I II III IV V VI VII VIII IX X
+## Variables:
+## $wallpaper Path to the wallpaper image
+## $desktops A space seperated list of desktop names
+## $sxhkd_extra_config A space seperated list of sxhkd extra config files
 
-picom --daemon
-pgrep -x sxhkd > /dev/null || sxhkd &
-
-nitrogen --set-scaled ~/.local/share/wallpapers/bspwm/boat.jpg
-~/.config/polybar/launch.sh --material &
-
-# like `bspwmrc` but not tracked by version control
+# Configure this config file
 [ -f $HOME/.config/bspwm/bspwmrc.local ] && . $HOME/.config/bspwm/bspwmrc.local;
 
-bspc config border_width         2
-bspc config window_gap          12
+# Defaults if not configured in the config file
+: ${wallpaper:=~/.local/share/wallpapers/bspwm/boat.jpg}
+: ${desktops:=I II III IV V VI VII VIII IX X}
+
+# Set monitor names (and count) according to the $desktops variable
+bspc monitor -d $desktops
+bspc config border_width            3
+bspc config window_gap             12
 
 bspc config split_ratio          0.52
 bspc config borderless_monocle   true
 bspc config gapless_monocle      true
 
-bspc rule -a "obs:obs:*" desktop="^3" follow=on
+# Start picom (composter) and sxhkd (key daemon)
+picom --daemon
+
+# If sxhkd is not running, start it
+if ! pgrep -x sxhkd ; then
+  sxhkd -c ~/.config/sxhkd/sxhkdrc $sxhkd_extra_config &
+fi
+
+# Set the wallpaper
+nitrogen --set-scaled $wallpaper
+
+# Start the polybar
+~/.config/polybar/launch.sh --material &
+
+# Launch obs in the fifth desktop
+bspc rule -a "obs:obs:*" desktop="^5" follow=on
